e746f911279f2da90ad99a922bb91136271f1d8e
Dingnuooo <dingnuooo@163.com>
2025-11-25 23:14:04 +0800

    :sparkles: feat: change waline comment server

--- src/site.config.ts
+++ src/site.config.ts
@@ -161,7 +161,8 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    // server: 'https://astro-theme-pure-waline.arthals.ink/',
+    server: 'https://comment.dingnuooo.top/',
     // Refer https://waline.js.org/en/guide/features/emoji.html
     emoji: ['bmoji', 'weibo'],
     // Refer https://waline.js.org/en/reference/client/props.html
